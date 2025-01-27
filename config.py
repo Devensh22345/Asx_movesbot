b227b8ebf1953092020928c5f2a4bd36e91885ee
Devensh22345 <kumardevensh0@gmail.com>
2025-01-27 16:49:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "<b>𝗔𝗻𝗶𝗺𝗲 𝗶𝗻 𝗛𝗶𝗻𝗱𝗶</b>")
+START_MSG = os.environ.get("START_MESSAGE", "<b>  𝗔𝗻𝗶𝗺𝗲 𝗶𝗻 𝗛𝗶𝗻𝗱𝗶 👇👇\nHTTPS://T.ME/+2FSV4NZHVOS2OGNL\nHTTPS://T.ME/+2FSV4NZHVOS2OGNL\n\n  𝗔𝗻𝗶𝗺𝗲 𝗖𝗵𝗮𝘁 𝗚𝗿𝗼𝘂𝗽 👇👇\nHTTPS://T.ME/DKANIME_GROUP\nHTTPS://T.ME/DKANIME_GROUP\n\n  𝗔𝗱𝘂𝗹𝘁 𝗔𝗻𝗶𝗺𝗲 𝗶𝗻 𝗵𝗶𝗻𝗱𝗶 [𝟭𝟴+] 😍👇👇\nhttps://t.me/+X-vfMcD-GkY3MzQ1\nhttps://t.me/+X-vfMcD-GkY3MzQ1</b>")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "7453879429").split()):
